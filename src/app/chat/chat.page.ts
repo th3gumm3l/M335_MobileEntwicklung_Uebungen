4decaae88cd463e927131f29c867c60c58d8cd58
Tim Bernhard <tim.bernhard@me.com>
2023-01-26 15:49:50 +0100

    avatar added

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -22,8 +22,7 @@ export class ChatPage implements OnInit {
 
   /*Autor Name und Profilbild*/
   currentAuthor: string = 'Tim';
-  currentAuthorAvatarImageUrl: string =
-    'https://www.w3schools.com/howto/img_avatar.png';
+  currentAuthorAvatarImageUrl: string = 'https://images.unsplash.com/photo-1663849172578-caaa4e6a6025?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
 
   constructor(private afs: Firestore, private alertCtrl: AlertController) {
     this.chatMessages$ = collectionData(
